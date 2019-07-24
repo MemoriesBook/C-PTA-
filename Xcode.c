b7077b040207964b5b2995cc0284515b8fdf0184
MemoriesBook <32359635+MemoriesBook@users.noreply.github.com>
2019-07-24 12:20:36 +0800

    Rename main.c to Xcode.c

---
+++ Xcode.c
@@ -0,0 +1,43 @@
+/*
+	 #include <stdio.h>
+	 int main()
+	 {
+	 int a,b,c,d;
+	 scanf("%d %d %d %d",&a,&b,&c,&d);
+	 printf("Sum = %d; Average = %.1f",a+b+c+d,(a+b+c+d)/4.0f);
+	 return 0;
+	 }
+	 #include <stdio.h>
+	 //#include <stdlib.h>
+	 int main()
+	 {
+	 signed int a,b;
+	 scanf("%d %d",&a,&b);
+	 printf("%d",a*b);
+	 //system("pause");
+	 return 0;
+	 }
+	 #include <stdio.h>
+	 //#include <stdlib.h>
+	 int main()
+	 {
+	 int H;
+	 100<H<=300;
+	 scanf("%d",&H);
+	 float W;
+	 W=(H-100)*0.9*2;
+	 printf("%.1f",W);
+	 //system("pause");
+	 return 0;
+	 }
+	 */
+	#include <stdio.h>
+	//#include <stdlib.h>
+	int main()
+	{
+	    int a;
+	    scanf("%d",&a);
+	    printf("%d,%d,%d",a/100,a/10%10,a%10);
+	    //system("pause");
+	    return 0;
+	}
